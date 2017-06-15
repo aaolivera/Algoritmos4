47ab6ff7ed3f3b2a1f8e544cdecaf7d4e65c21e3
Fiona Gonzalez Lisella <fiona.gonzalez@valtech.com>
2017-06-15 17:39:43 -0300

    Read indexed files

--- Tp2Ej1/filegenerator/filegenerator.cbl
+++ Tp2Ej1/filegenerator/filegenerator.cbl
@@ -12,45 +12,46 @@
 
 
        INPUT-OUTPUT SECTION.
-    		 FILE-CONTROL.
-  		   select CUITPROV
-		     assign to disk "CUITPROV.INFO"
+             FILE-CONTROL.
+             select CUITPROV
+             assign to disk "CUITPROV.INFO"
            organization is line sequential
            FILE STATUS IS fs-CUITPROV.
 
        select CUITPROVOUT
-		     assign to disk "CUITPROV.OUT"
-           organization INDEXED ACCESS DYNAMIC
-           RECORD KEY CUIT-CONS-OUT
-           ALTERNATE RECORD KEY COD-PROV-OUT WITH DUPLICATES
+             assign to disk "CUITPROV.OUT"
+           ORGANIZATION INDEXED
+           ACCESS MODE IS RANDOM
+           RECORD KEY COD-PROV-OUT
+           ALTERNATE RECORD KEY CUIT-CONS-OUT WITH DUPLICATES
            FILE STATUS IS fs-CUITPROVOUT.
 
        DATA DIVISION.
 
        FILE SECTION.
        fd CUITPROV.
-		     01 REG_CUITPROV.
-		     03 CUIT-CONS pic 9(15).
+             01 REG_CUITPROV.
+             03 CUIT-CONS pic 9(15).
          03 COD-PROV pic 9(08).
-		     03 FECHA-ALTA.
+             03 FECHA-ALTA.
             06 ANIO pic x(4).
             06 FILL pic x(6).
 
        fd CUITPROVOUT.
-		     01 REG_CUITPROVOUT.
-		     03 CUIT-CONS-OUT pic 9(15).
+             01 REG_CUITPROVOUT.
+             03 CUIT-CONS-OUT pic 9(15).
          03 COD-PROV-OUT pic 9(08).
-		     03 FECHA-ALTA.
+             03 FECHA-ALTA.
             06 ANIO pic x(4).
             06 FILL pic x(6).
 
        WORKING-STORAGE SECTION.
        01 fs-CUITPROV pic xx.
-		     88 ok-CUITPROV value "00".
-		     88 eof-CUITPROV value "10".
+             88 ok-CUITPROV value "00".
+             88 eof-CUITPROV value "10".
         01 fs-CUITPROVOUT pic xx.
-		     88 ok-CUITPROVOUT value "00".
-		     88 eof-CUITPROVOUT value "10".
+             88 ok-CUITPROVOUT value "00".
+             88 eof-CUITPROVOUT value "10".
 
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
--- Tp2Ej1/tp2ej1.cbl
+++ Tp2Ej1/tp2ej1.cbl
@@ -4,12 +4,64 @@
        environment division.
        configuration section.
 
+       INPUT-OUTPUT SECTION.
+           FILE-CONTROL.
+               select CUITPROV
+                   assign to disk "CUITPROV.OUT"
+                   organization INDEXED ACCESS DYNAMIC
+                   RECORD KEY COD-PROV
+                   ALTERNATE RECORD KEY CUIT-CONS WITH DUPLICATES
+               FILE STATUS IS fs-CUITPROV.
+
        data division.
-       working-storage section.
+
+
+       FILE SECTION.
+           fd CUITPROV.
+           01 REG_CUITPROV.
+               03 CUIT-CONS pic 9(15).
+               03 COD-PROV pic 9(08).
+               03 FECHA-ALTA.
+               06 ANIO pic x(4).
+               06 FILL pic x(6).
+
+       WORKING-STORAGE SECTION.
+           01 fs-CUITPROV pic xx.
+               88 ok-CUITPROV value "00".
+               88 eof-CUITPROV value "10".
+
+           01 CLAVE pic 9(08).
+           01 COUNTER pic 9(10).
+           01 eof-file pic 9(1).
+
+
 
        procedure division.
+           OPEN INPUT CUITPROV.
 
+           MOVE 3 to CLAVE.
+           MOVE 0 to COUNTER.
 
-           goback.
+           START CUITPROV
+               KEY IS GREATER THAN CLAVE
+               INVALID KEY DISPLAY "error no hay clave"
+               NOT INVALID KEY DISPLAY "clave ok"
+           END-START.
+
+           READ CUITPROV NEXT RECORD
+               AT END SET eof-CUITPROV TO TRUE
+           END-READ.
+
+      *>      PERFORM UNTIL eof-CUITPROV
+      *>          READ CUITPROV RECORD
+      *>              add 1 to COUNTER
+      *>          END-READ
+      *>      END PERFORM.
+
+           CLOSE CUITPROV.
+       STOP RUN.
+
+       contar.
+           add 1 to COUNTER.
 
        end program tp2ej1.

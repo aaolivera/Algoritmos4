b2a464d77216d55c3e2210d139ff4117b1aea8ce
Fiona Gonzalez Lisella <fiona.gonzalez@valtech.com>
2017-06-21 15:45:06 -0300

    cambios en tp para compilar gnuCOBOL + ejemplo de actualizarProv

--- Tp2Ej1/tp2ej1.cbl
+++ Tp2Ej1/tp2ej1.cbl
@@ -29,38 +29,42 @@
            01 fs-CUITPROV pic xx.
                88 ok-CUITPROV value "00".
                88 eof-CUITPROV value "10".
-               
-           01 tablaConteo occurs 99999999 times.
-			    03 COUNTER pic 9(10).
+
+           01 tablaConteo.
+               03 COUNTER OCCURS 99999999 TIMES pic 9(10).
            01 COUNTERAUX pic 9(10).
+
        procedure division.
            OPEN INPUT CUITPROV.
 
+           CALL 'actualizarProv' USING tablaConteo.
+
            READ CUITPROV NEXT RECORD.
            PERFORM contar UNTIL eof-CUITPROV.
-           
-           CALL actualizarProv tablaConteo.
-           
+
+
+
            CLOSE CUITPROV.
        STOP RUN.
 
        contar.
-           MOVE tablaConteo(COD-PROV of REG_CUITPROV) TO 
-           COUNTERAUX
+           MOVE COUNTER(COD-PROV of REG_CUITPROV) TO COUNTERAUX.
+
            ADD 1 to COUNTERAUX.
-           MOVE  COUNTERAUX TO tablaConteo(COD-PROV of REG_CUITPROV)
+
+           MOVE  COUNTERAUX TO COUNTER(COD-PROV of REG_CUITPROV).
            READ CUITPROV NEXT RECORD.
-       
-       actualizarProv.
-           OPEN output PROV
-           MOVE 1 TO i
-           PERFORM actualizar UNTIL i == 10000000.
-           CLOSE PROV.
-       actualizar.
-           if(tablaconteo(i) > 0)
-               READ PROV RECORD KEY IS i
-               MOVE tablaConteo(i) TO PRO_REG_CANT_CONS
-               REWRITE PROV_REG.
-           ADD 1 TO i.
-           
+
+      *>  actualizarProv.
+      *>      OPEN OUTPUT PROV.
+      *>      MOVE 1 TO i
+      *>      PERFORM actualizar UNTIL i == 10000000.
+      *>      CLOSE PROV.
+      *>  actualizar.
+      *>      if(tablaconteo(i) > 0)
+      *>          READ PROV RECORD KEY IS i
+      *>          MOVE tablaConteo(i) TO PRO_REG_CANT_CONS
+      *>          REWRITE PROV_REG.
+      *>      ADD 1 TO i.
+
        end program tp2ej1.

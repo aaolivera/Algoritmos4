42a98511fb098ee2a3a1c94cacb362a0f540a141
Fiona Gonzalez Lisella <fiona.gonzalez@valtech.com>
2017-06-21 16:56:14 -0300

    agrego a fileGenerator todos los campos del registro

--- Tp2Ej1/tp2ej1.cbl
+++ Tp2Ej1/tp2ej1.cbl
@@ -8,7 +8,7 @@
            FILE-CONTROL.
                select CUITPROV
                    assign to disk "CUITPROV.OUT"
-                   organization INDEXED ACCESS DYNAMIC
+                   organization INDEXED ACCESS SEQUENTIAL
                    RECORD KEY COD-PROV
                    ALTERNATE RECORD KEY CUIT-CONS WITH DUPLICATES
                FILE STATUS IS fs-CUITPROV.
@@ -20,10 +20,10 @@
            fd CUITPROV.
            01 REG_CUITPROV.
                03 CUIT-CONS pic 9(15).
-               03 COD-PROV pic 9(08).
+               03 COD-PROV pic 9(8).
                03 FECHA-ALTA.
-               06 ANIO pic x(4).
-               06 FILL pic x(6).
+                   06 ANIO pic x(4).
+                   06 FILL pic x(6).
 
        WORKING-STORAGE SECTION.
            01 fs-CUITPROV pic xx.
@@ -31,28 +31,33 @@
                88 eof-CUITPROV value "10".
 
            01 tablaConteo.
-               03 COUNTER OCCURS 99999999 TIMES pic 9(10).
+               03 COUNTER OCCURS 99999999 TIMES pic 9(08).
            01 COUNTERAUX pic 9(10).
 
        procedure division.
            OPEN INPUT CUITPROV.
 
-           CALL 'actualizarProv' USING tablaConteo.
+           MOVE 0 TO COUNTERAUX.
 
            READ CUITPROV NEXT RECORD.
            PERFORM contar UNTIL eof-CUITPROV.
 
-
+           DISPLAY COUNTER(3).
+           CALL  "actualizarProv" USING tablaConteo.
 
            CLOSE CUITPROV.
        STOP RUN.
 
        contar.
-           MOVE COUNTER(COD-PROV of REG_CUITPROV) TO COUNTERAUX.
+           DISPLAY COD-PROV of REG_CUITPROV.
+           DISPLAY CUIT-CONS of REG_CUITPROV.
+           DISPLAY FECHA-ALTA of REG_CUITPROV.
+      *>      MOVE COUNTER(COD-PROV of REG_CUITPROV) TO COUNTERAUX.
 
            ADD 1 to COUNTERAUX.
+           DISPLAY COUNTERAUX.
 
-           MOVE  COUNTERAUX TO COUNTER(COD-PROV of REG_CUITPROV).
+      *>      MOVE  COUNTERAUX TO COUNTER(COD-PROV of REG_CUITPROV).
            READ CUITPROV NEXT RECORD.
 
       *>  actualizarProv.

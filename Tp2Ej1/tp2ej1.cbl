150e5b00e39068be0026652a4870c62e2ff6e570
Alejandro Olivera <aa.olivera@hotmail.com>
2017-07-02 04:42:22 -0200

    Repaso general
    Informe y pracicos alineados
    
    =! falta manejar paginado en reporte

--- Tp2Ej1/tp2ej1.cbl
+++ Tp2Ej1/tp2ej1.cbl
@@ -18,13 +18,6 @@
                    organization is line sequential
                    file status is fs-MAESTRO.
 
-               select PROV
-                   assign to disk "PROV.OUT"
-                   ORGANIZATION INDEXED
-                   ACCESS MODE is RANDOM
-                   RECORD KEY IS COD-PROV of REG_PROV
-                   FILE STATUS IS fs-PROV.
-
                SELECT ORDENAR
                    ASSIGN TO "WORK.TMP"
                    FILE status is fs-ORDENAR.
@@ -51,20 +44,10 @@
                    03 DIR pic x(30).
                    03 NRO-CTA pic 9(8).
 
-           fd PROV.
-               01 REG_PROV.
-                   03 COD-PROV pic 9(8).
-                   03 DIR pic 9(30).
-                   03 TEL pic 9(15).
-                   03 RUBRO pic X(4).
-                   03 DESC-RUBRO pic X(15).
-                   03 FECHA-ALTA pic 9(8).
-                   03 CANT pic 9(3).
-
            SD ORDENAR.
                01 REG_ORDENAR.
                    03 RUBRO pic x(4).
-                   03 COD-PROV-O pic 9(8).
+                   03 COD-PROV pic 9(8).
                    03 DESC pic X(15).
                    03 CUIT-CONS pic 9(15).
                    03 NOMBRE-CONSORCIO pic x(30).
@@ -90,22 +73,40 @@
 
            01 tablaConteo.
                03 COUNTER pic 9(08) VALUE 0 OCCURS 99999999 TIMES.
-
+           
+           01 REG_PROV.
+                   03 COD-PROV pic 9(8).
+                   03 DIR pic 9(30).
+                   03 TEL pic 9(15).
+                   03 RUBRO pic X(4).
+                   03 DESC-RUBRO pic X(15).
+                   03 FECHA-ALTA pic 9(8).
+                   03 CANT pic 9(3).
+               
+           
            01 COUNTERAUX pic 9(08).
-           01 CONSOR_ITERATOR pic 9(15) VALUE 85.
-
            01 WS-YYYY-MM-DD pic 9(8).
            01 PAGINAS pic 99.
            01 LINEAS pic 9(8).
-           01 LINEASAAGREGAR pic 9(8).
-
-           01 FLAG pic 9(1) VALUE 0.
            01 RUBRO_ACT pic x(4).
-           01 CONTADOR_RUBRO pic 9(2).
+           01 COD-PROV_ACT pic 9(8).
+           01 CONTADOR_PROV_POR_RUBRO pic 9(2).
            01 CONTADOR_TOTAL_RUBRO pic 9(2).
 
        procedure division.
            display "|--- Iniciando Tp2 ---|".
+           
+           perform ContarConsorciosPorProveedor.
+           
+           CALL "PRO" USING "abr".
+           CALL "PRO" USING "act" tablaConteo.
+           
+           perform GenerarListado.
+
+           CALL "PRO" USING "cer".
+       STOP RUN.
+       
+       ContarConsorciosPorProveedor.
            display "--- Procesando archivo CUITPROV ---".
            OPEN input CUITPROV.
            READ CUITPROV NEXT RECORD.
@@ -113,23 +114,9 @@
            CLOSE CUITPROV.
            display "--- Fin CUITPROV ---".
            
-           CALL  "actualizarProv" USING tablaConteo.
-           
-           display "--- Generación de listado por rubro ---".
-           MOVE 1 to PAGINAS.
-           MOVE 0 to LINEAS.
-           SORT ORDENAR
-               ON ASCENDING RUBRO of REG_ORDENAR
-               ON ASCENDING COD-PROV-O of REG_ORDENAR
-               ON ASCENDING CUIT-CONS of REG_ORDENAR
-           INPUT PROCEDURE is ENTRADA
-           OUTPUT PROCEDURE is SALIDA.
-
-       STOP RUN.
-
        CONTAR.
            display "--CUIT-CONS "CUIT-CONS of REG_CUITPROV" COD-PROV "
-           COD-PROV of REG_CUITPROV.
+           COD-PROV of REG_CUITPROV" ".
 
            MOVE COUNTER(COD-PROV of REG_CUITPROV) TO COUNTERAUX.
 
@@ -138,63 +125,99 @@
            MOVE  COUNTERAUX TO COUNTER(COD-PROV of REG_CUITPROV).
            READ CUITPROV NEXT RECORD.
 
+       GenerarListado.
+           display "---------------------------------------".
+           display "--- Generación de listado por rubro ---".
+           display "---------------------------------------"..
+           MOVE 1 to PAGINAS.
+           MOVE 0 to LINEAS.
+           SORT ORDENAR
+               ON ASCENDING RUBRO of REG_ORDENAR
+               ON ASCENDING COD-PROV of REG_ORDENAR
+               ON ASCENDING CUIT-CONS of REG_ORDENAR
+           INPUT PROCEDURE is ENTRADA
+           OUTPUT PROCEDURE is SALIDA.
+           
        ENTRADA.
-           OPEN INPUT PROV.
            OPEN INPUT CUITPROV.
-
-           READ CUITPROV NEXT RECORD.
-           PERFORM LOAD_RECORD UNTIL eof-CUITPROV.
+           OPEN INPUT MAESTRO.
+           
+           READ MAESTRO.
+           READ CUITPROV.
+           PERFORM LOAD_MAE UNTIL eof-MAESTRO or eof-CUITPROV.
            CLOSE CUITPROV.
+           CLOSE MAESTRO.
+           
+       LOAD_MAE.
+           PERFORM LOAD_CUITPROV UNTIL eof-CUITPROV or
+           (CUIT-CONS of REG_CUITPROV) > (CUIT-CO of REG_MAESTRO)
+       
+           READ MAESTRO.
+       
+       LOAD_CUITPROV.
+           move COD-PROV of REG_CUITPROV to REG_PROV.
+           CALL "PRO" USING "lee" null REG_PROV.
+       
+           MOVE CUIT-CO of REG_MAESTRO TO CUIT-CONS of REG_ORDENAR
+           MOVE NOMBRE-CONSORCIO of REG_MAESTRO TO NOMBRE-CONSORCIO
+                   of REG_ORDENAR
+           MOVE TEL of REG_MAESTRO to TEL of REG_ORDENAR
+           MOVE DIR OF REG_MAESTRO to DIR of REG_ORDENAR
 
+           MOVE DESC-RUBRO of REG_PROV to DESC of REG_ORDENAR.
+           MOVE RUBRO of REG_PROV to RUBRO of REG_ORDENAR.
+               
+           MOVE COD-PROV of REG_CUITPROV TO COD-PROV of REG_ORDENAR
+                                                                                
+           release REG_ORDENAR
+           READ CUITPROV NEXT RECORD.
+       
+           
        SALIDA.
+           RETURN ORDENAR.
+               
            PERFORM EMITIR_ENCABEZADO.
-           RETURN ORDENAR RECORD INTO REG_ORDENAR
-               AT END DISPLAY " ".
-           MOVE RUBRO of REG_ORDENAR to RUBRO_ACT.
+           PERFORM EMITIR_ENCABEZADO_RUBRO.
+           
            MOVE 1 TO CONTADOR_TOTAL_RUBRO.
-           DISPLAY "RUBRO: " RUBRO_ACT"  DESCRIPCION: "
-           DESC OF REG_ORDENAR.
-           MOVE 0 TO CONTADOR_RUBRO.
-           display
-               "COD_PROV   CUIT-CONSORCIO   NOMBRE-CONSORCIO   TEL-CONS"
-               "   DIR-CONS".
+           MOVE 1 TO CONTADOR_PROV_POR_RUBRO.
+           
+           MOVE RUBRO of REG_ORDENAR to RUBRO_ACT.
+           move COD-PROV of REG_ORDENAR to COD-PROV_ACT 
+           
            PERFORM PROCESAR_ORDENADO until eof-ORDENAR.
-           DISPLAY "TOTAL DE PROVEEDORES POR RUBRO: " CONTADOR_RUBRO
-           DISPLAY "TOTAL RUBROS: " CONTADOR_TOTAL_RUBRO.
+           PERFORM EMITIR_TOTAL_PROOVEEDORES_POR_RUBRO.
+           PERFORM EMITIR_TOTAL_RUBRO.
 
        PROCESAR_ORDENADO.
-           PERFORM VALIDAR_PAGINA.
-
            IF RUBRO_ACT IS EQUAL TO RUBRO of REG_ORDENAR
-               ADD 1 TO CONTADOR_RUBRO
-           ELSE
-               DISPLAY "TOTAL DE PROVEEDORES POR RUBRO: " CONTADOR_RUBRO
+               AND COD-PROV_ACT is not equal to COD-PROV of REG_ORDENAR
+                   ADD 1 TO CONTADOR_PROV_POR_RUBRO
+                   move COD-PROV of REG_ORDENAR to COD-PROV_ACT.
+           
+           IF RUBRO_ACT IS NOT EQUAL TO RUBRO of REG_ORDENAR
+               PERFORM EMITIR_TOTAL_PROOVEEDORES_POR_RUBRO
+               PERFORM EMITIR_TOTAL_RUBRO
                DISPLAY " "
-               MOVE 0 TO CONTADOR_RUBRO
+               PERFORM EMITIR_ENCABEZADO_RUBRO
+               MOVE 0 TO CONTADOR_PROV_POR_RUBRO
                ADD 1 TO CONTADOR_TOTAL_RUBRO
-               MOVE RUBRO of REG_ORDENAR to RUBRO_ACT
+               MOVE RUBRO of REG_ORDENAR to RUBRO_ACT.
 
-               DISPLAY "RUBRO: " RUBRO_ACT "    DESCRIPCION: "
-               DESC of REG_ORDENAR
-               DISPLAY " "
-               ADD 4 TO LINEAS
-               display
-               "COD_PROV   CUIT-CONSORCIO   NOMBRE-CONSORCIO   TEL-CONS"
-               "   DIR-CONS".
-           
-           
-           DISPLAY COD-PROV-O of REG_ORDENAR"   "
+           DISPLAY COD-PROV of REG_ORDENAR"   "
                    CUIT-CONS of REG_ORDENAR " "
                    NOMBRE-CONSORCIO of REG_ORDENAR " "
+                   TEL OF REG_ORDENAR " "
                    DIR of REG_ORDENAR.
            ADD 1 TO LINEAS.
-           RETURN ORDENAR RECORD INTO REG_ORDENAR
-               AT END DISPLAY " ".
+           
+           RETURN ORDENAR RECORD INTO REG_ORDENAR.
 
        VALIDAR_PAGINA.
            if(LINEAS + 1 > 60)
                add 1 to PAGINAS
                perform EMITIR_ENCABEZADO.
+               
        EMITIR_ENCABEZADO.
            accept WS-YYYY-MM-DD from date yyyymmdd.
            display "Fecha: "WS-YYYY-MM-DD"                             "
@@ -204,46 +227,17 @@
            move 3 to LINEAS.
 
 
-       LOAD_RECORD.
-           MOVE COD-PROV of REG_CUITPROV TO COD-PROV-O of REG_ORDENAR
-           MOVE CUIT-CONS of REG_CUITPROV TO CUIT-CONS of REG_ORDENAR
-
-           MOVE COD-PROV-O to COD-PROV of REG_PROV
-               READ PROV
-                   INVALID KEY DISPLAY "ERROR"
-               END-READ
-           MOVE DESC-RUBRO of REG_PROV to DESC of REG_ORDENAR
-
-
-           OPEN INPUT MAESTRO.
-           READ MAESTRO.
-           PERFORM BUSCAR_DATA_MAESTRO UNTIL (FLAG is EQUAL 1
-               OR eof-MAESTRO).
-           MOVE 0 to FLAG.
-           CLOSE MAESTRO.
-
-           PERFORM BUSCAR_DATA_PROV.
-           release REG_ORDENAR
-           READ CUITPROV NEXT RECORD.
-
-
-       BUSCAR_DATA_MAESTRO.
-           if (CUIT-CONS of REG_CUITPROV) =
-               (CUIT-CO of REG_MAESTRO)
-               MOVE NOMBRE-CONSORCIO of REG_MAESTRO TO NOMBRE-CONSORCIO
-                   of REG_ORDENAR
-               MOVE TEL of REG_MAESTRO to TEL of REG_ORDENAR
-               MOVE DIR OF REG_MAESTRO to DIR of REG_ORDENAR
-               MOVE 1 TO FLAG.
-           READ MAESTRO.
-
-       BUSCAR_DATA_PROV.
-
-           OPEN INPUT PROV.
-           MOVE COD-PROV of REG_CUITPROV TO COD-PROV of REG_PROV.
-
-           READ PROV.
-           MOVE RUBRO of REG_PROV to RUBRO of REG_ORDENAR.
-           CLOSE PROV.
-
-       end program tp2ej1.
+       EMITIR_ENCABEZADO_RUBRO.
+       DISPLAY "RUBRO: " RUBRO of REG_ORDENAR "  DESCRIPCION: "
+           DESC OF REG_ORDENAR.
+       DISPLAY
+               "COD_PROV   CUIT-CONSORCIO  NOMBRE-CONSORCIO            "
+               "   TEL-CONS        DIR-CONS".
+       move 3 to LINEAS.
+       EMITIR_TOTAL_PROOVEEDORES_POR_RUBRO.
+       DISPLAY "TOTAL DE PROVEEDORES POR RUBRO: "
+               CONTADOR_PROV_POR_RUBRO.
+       move 1 to LINEAS.
+       EMITIR_TOTAL_RUBRO.
+       DISPLAY "TOTAL RUBROS: " CONTADOR_TOTAL_RUBRO.
+       move 1 to LINEAS.
